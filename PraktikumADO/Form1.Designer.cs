9e2ff3abaeec023d2dac5084f6eceffdb8af34ea
Ghatan55 <ghatanhardanni@gmail.com>
2026-03-31 10:09:16 +0700

    Memperbaiki kesalahan nama Tabel Program Studi

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -130,7 +130,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1128, 450);
             this.Controls.Add(this.txtHasil);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnInsertProdi);
