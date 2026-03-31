bd3332e8d7dab2fc1bdb041937a9111a827bf1f9
Ghatan55 <ghatanhardanni@gmail.com>
2026-03-31 09:48:47 +0700

    Menerapkan logika try-catch pada tombol koneksi untuk pengujian akses database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -40,5 +40,20 @@ namespace PraktikumADO
         {
 
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil!");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
